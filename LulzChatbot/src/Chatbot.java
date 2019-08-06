b21a94ae8a1711a5c47571eb5284c966edc2430a
Joshua Dapitan <joshuadapitan@gmail.com>
2019-08-06 21:00:32 +0800

    support clear button

--- LulzChatbot/src/Chatbot.java
+++ LulzChatbot/src/Chatbot.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Chatbot extends JFrame implements ActionListener,KeyListener{
-	TextArea taChatLog=new TextArea(13,50);
+public class Chatbot extends JFrame implements ActionListener, KeyListener{
+	TextArea taChatLog = new TextArea(13,50);
 	static JTextField tfMessage=new JTextField(25);
-	JButton btSend=new JButton("Send");
-	JButton shit=new JButton("Ask me a question and I will give an answer.");
-	Label lbM=new Label("Message:");
+	JButton btSend = new JButton("Send");
+	JButton btClear = new JButton("Clear");
+	JButton magicalContainer = new JButton("Ask me a question and I will give an answer.");
+	Label lbM = new Label("Message:");
 	String s="";
 	Chatbot(){
 		setTitle("Chatbot");
@@ -15,7 +16,7 @@ public class Chatbot extends JFrame implements ActionListener,KeyListener{
 		setLayout(new FlowLayout());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-		add(shit);
+		add(magicalContainer);
 		add(taChatLog);
 		taChatLog.setEditable(false);
 		taChatLog.setBackground(Color.WHITE);
@@ -24,24 +25,28 @@ public class Chatbot extends JFrame implements ActionListener,KeyListener{
 		tfMessage.addActionListener(this);
 		add(btSend);
 		btSend.addActionListener(this);
+		add(btClear);
+		btClear.addActionListener(this);
 		tfMessage.requestFocus();
-		shit.addKeyListener(this);
+		magicalContainer.addKeyListener(this);
 		tfMessage.addKeyListener(this);
 	}
 	public void keyTyped(KeyEvent e) {
-		if(e.getKeyChar()=='?'&e.getSource()==tfMessage){
-			shit.requestFocus();
-			s="";
+		if(e.getKeyChar() == '?' & e.getSource() == tfMessage) {
+			magicalContainer.requestFocus();
+			s = "";
 		}
-                else if(e.getSource()==shit&e.getKeyChar()=='\n')
-                {
-                    actionPerformed(new ActionEvent(btSend,0,null));
-                }
-		else if(e.getSource()==shit&e.getKeyChar()!='\b')
-			s=s+e.getKeyChar();
-		else{
+
+		else if(e.getSource() == magicalContainer & e.getKeyChar() == '\n') {
+			actionPerformed(new ActionEvent(btSend, 0, null));
+		}
+		
+		else if(e.getSource() == magicalContainer & e.getKeyChar() != '\b')
+			s = s + e.getKeyChar();
+		
+		else {
 			tfMessage.requestFocus();
-			s="";
+			s = "";
 		}
 	}
 	public void keyPressed(KeyEvent e) {
@@ -51,9 +56,14 @@ public class Chatbot extends JFrame implements ActionListener,KeyListener{
 
 	}
 	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btClear){
+			tfMessage.setText("");
+			taChatLog.setText("");
+			s = "";
+		}
 		if(e.getSource()==btSend){
-			taChatLog.setText(taChatLog.getText()+"You:"+tfMessage.getText()+"\nChatbot:"+ChopChop(s)+'\n');
-			s="";
+			taChatLog.setText(taChatLog.getText() + "You:" + tfMessage.getText() + "\nChatbot:" + ChopChop(s)+ '\n');
+			s = "";
 			tfMessage.requestFocus();
 			tfMessage.setText("");
 		}
@@ -61,18 +71,18 @@ public class Chatbot extends JFrame implements ActionListener,KeyListener{
 	public static String ChopChop(String s){
 		String temp="";
 		if(!tfMessage.getText().endsWith("?"))return "Is that a question?";
-		if(s.length()==0)return "I don't know";
+		if(s.length() == 0)return "I don't know";
 		if(s.equalsIgnoreCase("o"))return "Oo";
 		if(s.equalsIgnoreCase("y"))return "Yes";
 		if(s.equalsIgnoreCase("n"))return "No";
 		if(s.equalsIgnoreCase("h"))return "Hindi";
-		for(int x=0;x<s.length();x++){
+		for(int x = 0;x < s.length(); x++){
 			if(Character.isDefined(s.charAt(x)))
-				temp=temp+s.charAt(x);
+				temp = temp + s.charAt(x);
 		}
 		return temp;
 	}
 	public static void main(String[]args){
-		Chatbot c=new Chatbot();
+		Chatbot c = new Chatbot();
 	}
 }
